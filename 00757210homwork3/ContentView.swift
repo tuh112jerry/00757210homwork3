259ba7fcc25f646c36b74724c3364a5ba44e640e
moonsword5083 <anthea880702@gmail.com>
2020-11-04 02:08:09 +0800

    Initial Commit

---
+++ 00757210homwork3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757210homwork3
+//
+//  Created by User23 on 2020/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757210homwork3/_0757210homwork3App.swift
@@ -0,0 +1,17 @@
+//
+//  _0757210homwork3App.swift
+//  00757210homwork3
+//
+//  Created by User23 on 2020/11/4.
+//
+
+import SwiftUI
+
+@main
+struct _0757210homwork3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
